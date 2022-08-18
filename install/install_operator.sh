10bbba79f1a59275623bd1d59237e8e11ce10b26
Marcus <111141205+ElagabalxNode@users.noreply.github.com>
2022-08-18 21:23:16 +0300

    1

--- install/install_operator.sh
+++ install/install_operator.sh
@@ -67,7 +67,7 @@ install_operator () {
   #ls -lah ./
 
   echo -e "\e[1m\e[32mInstall NeonEVM Operator\e[0m"
-  ansible-playbook --connection=local --i ./inventory/devnet.yml --limit local playbooks/install.yml --extra-vars "{ \
+  ansible-playbook --connection=local --inventory ./inventory/devnet.yml --limit local playbooks/install.yml --extra-vars "{ \
   'neonevm_solana_rpc': '$rpc_var', \
   'postgres_db': 'neon-db', \
   'neonevm_user': '$neonevm_user', \
